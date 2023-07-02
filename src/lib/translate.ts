105527485a0c77dbf3001aefc16792e0f8e155d2
nazbeer <5653844+nazbeer@users.noreply.github.com>
2023-07-02 13:21:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/lib/translate.ts
@@ -0,0 +1,11 @@
+// import en_translations from '../translations/en.json';
+// import ru_translations from '../translations/ru.json';
+// function getTranslations(lang: 'en'|'ru') {
+//   if (lang === 'en') {
+//     return en_translations;
+//   }
+//   else if(lang === 'ru') {
+//    return ru_translations;
+//  }
+// }
+   
